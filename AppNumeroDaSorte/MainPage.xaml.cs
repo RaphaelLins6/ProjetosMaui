cd8bb83d699845073143d9d93b21c37515f119fd
Raphael Lins <rapharell06@gmail.com>
2025-03-20 23:34:09 -0300

    Update

--- AppNumeroDaSorte/MainPage.xaml.cs
+++ AppNumeroDaSorte/MainPage.xaml.cs
@@ -4,4 +4,9 @@ public partial class MainPage : ContentPage {
     public MainPage() {
         InitializeComponent();
     }
+
+    protected override void OnAppearing() {
+        base.OnAppearing();
+        Console.WriteLine("MainPage apareceu");
+    }
 }
