93dfcccd216db614fde3fd203c114f1d735c2a78
Raphael Lins <rapharell06@gmail.com>
2025-04-17 10:11:18 -0300

    Salvando progresso do app 6

--- AppMAUIGaleria/Views/Menu.xaml.cs
+++ AppMAUIGaleria/Views/Menu.xaml.cs
@@ -1,3 +1,5 @@
+using AppMAUIGaleria.Reps;
+
 namespace AppMAUIGaleria.Views;
 
 public partial class Menu : ContentPage
@@ -5,5 +7,28 @@ public partial class Menu : ContentPage
 	public Menu()
 	{
 		InitializeComponent();
+
+		var categorias = new CategoriaReps().GetCategorias;
+
+		var categorialbl = new Label();
+		
+		foreach (var categoria in categorias()) 
+		{
+            categorialbl.Text += categoria.Name + "\n";
+
+            MenuContainer.Children.Add(categorialbl);
+
+            foreach (var componente in categoria.Componentes) 
+			{
+				var categorialblTitulo = new Label();
+                categorialblTitulo.Text += "  " + componente.Titulo + "\n";
+
+                var categorialblDescricao = new Label();
+                categorialblDescricao.Text += "    " + componente.Descricao + "\n";
+
+                MenuContainer.Children.Add(categorialblTitulo);
+                MenuContainer.Children.Add(categorialblDescricao);
+            }
+        }
 	}
 }
