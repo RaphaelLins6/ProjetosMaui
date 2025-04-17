42dc7ca8c2b82ffe7b86b635fce0b130a10ca2de
Raphael Lins <rapharell06@gmail.com>
2025-04-17 13:13:25 -0300

    Atualização de progresso - App 6

--- AppMAUIGaleria/Views/Menu.xaml.cs
+++ AppMAUIGaleria/Views/Menu.xaml.cs
@@ -20,15 +20,34 @@ public partial class Menu : ContentPage
 
             foreach (var componente in categoria.Componentes) 
 			{
+                var tap = new TapGestureRecognizer();
+                tap.CommandParameter = componente.Pagina;
+                tap.Tapped += TapGestureRecognizer_Tapped;
+
 				var categorialblTitulo = new Label();
                 categorialblTitulo.Text += "  " + componente.Titulo + "\n";
+				categorialblTitulo.FontFamily = "OpenSansSemibold";
+				categorialblTitulo.Margin = new Thickness(20, 10, 0, 0);
+                categorialblTitulo.GestureRecognizers.Add(tap);
 
                 var categorialblDescricao = new Label();
                 categorialblDescricao.Text += "    " + componente.Descricao + "\n";
+                categorialblDescricao.Margin = new Thickness(20, 0, 0, 0);
+                categorialblDescricao.GestureRecognizers.Add(tap);
 
                 MenuContainer.Children.Add(categorialblTitulo);
                 MenuContainer.Children.Add(categorialblDescricao);
             }
         }
 	}
+
+    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e) 
+    {
+        var label = (Label)sender;
+        var tap = (TapGestureRecognizer)label.GestureRecognizers[0];
+        var page = (Page)tap.CommandParameter;
+
+        ((FlyoutPage)App.Current.MainPage).Detail = page;
+        ((FlyoutPage)App.Current.MainPage).IsPresented = false;
+    }
 }
